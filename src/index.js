b3fe0da77412bb895bfdedb66a54e2afc8c28b06
Aria Zhang <zmzhangg@gmail.com>
2023-01-13 14:03:23 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <li>Bacon</li>
+    <li>Jamon</li>
+    <li>Noodles</li>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
