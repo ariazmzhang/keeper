8dcf546f8d1672b7fd4284d0167762ead4490126
Aria Zhang <ariazhang@dhcp-135-170.cu-wireless-students.carleton.ca>
2023-01-13 16:44:05 -0500

    add React components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-//Create a react app from scratch.
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(
-  <div>
-    <h1>My Favourite Foods</h1>
-    <li>Bacon</li>
-    <li>Jamon</li>
-    <li>Noodles</li>
-  </div>,
-  document.getElementById("root")
-);
 
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+ReactDOM.render(<App />, document.getElementById("root"))
+
+
+
+
