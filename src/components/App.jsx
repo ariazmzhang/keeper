ebd80d4c2f8b50682d3d2215b24774306d69f3b5
Aria Zhang <ariazhang@Arias-MBP.ht.home>
2023-01-15 23:14:28 -0500

    add React state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import CreateArea
+ from "./CreateArea";
 function App(){
-    return <div>
+
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+            return (
+                [... prevNotes,newNote]
+            )
+        })
+       
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+            prevNotes.filter((noteItem, index)=> {
+                return index != id;
+            })
+        })
+    }
+    return (<div>
        <Header />
-       <Note />
+       <CreateArea onAdd={addNote}/>
+       {notes.map((noteItem, index) => {
+        return (
+            <Note 
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+       />
+        );
+       })}
        <Footer />
     </div>
+       );
 }
 
 export default App;
